5775b401a2865e832e1ccb3196d16f74792677de
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2020-05-23 21:13:23 +0700

    add categories controller

---
+++ controllers/category.js
@@ -0,0 +1,30 @@
+const Category = require('../models/category')
+const { httpCodes } = require('../utils/constant');
+const { Exception } = require('../utils');
+
+const addCategory = async (req, res, next) =>  {
+    try {
+        const { name } = req.body
+        if(name) throw new Exception('invalid name')
+        const category = new Category({name})
+        await category.save()
+        res.status(httpCodes.OK).send({category})
+    } catch (error) {
+        next(error)
+    }
+}
+
+const getAllCategories = async (req, res, next) => {
+    try {
+        const categories = await Category.find({})
+        if(categories) throw new Exception('categories not found', httpCodes.NOT_FOUND)
+        res.status(httpCodes.OK).send({categories})        
+    } catch(error) {
+        next(error)
+    }
+}
+
+module.exports = {
+    addCategory,
+    getAllCategories
+}
