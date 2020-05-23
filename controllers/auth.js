10bc5ccf9a1682bf8fa58d7a9c3ff82bff8ce1d9
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2020-05-23 21:08:00 +0700

    add user, auth controller

---
+++ controllers/auth.js
@@ -0,0 +1,30 @@
+const { generateAccessToken } = require('../utils/jwt');
+const { verifyPassword, Exception } = require('../utils');
+const User = require('../models/user.model');
+const { env } = require('../config/globals');
+const { httpCodes } = require('../utils/constant');
+
+const login = async (req, res, next) => {
+	try {
+		const { username, password } = req.body;
+		if (!username || !password) throw new Exception('username or password incorrect');
+		const user = await User.findOne({ username });
+		if (!user) throw new Exception('username or password incorrect');
+		const isValidPassword = await verifyPassword(user.password, password);
+		if (!isValidPassword) throw new Exception('username or password incorrect');
+		delete user._doc.password;
+		const { avatarUrl } = user._doc;
+		const token = await generateAccessToken(
+			{ _id: user._id, username: user._doc.username, avatarUrl },
+			env.JWT_SECRET_KEY,
+			'7d'
+		);
+		res.status(httpCodes.OK).send({ user, token });
+	} catch (error) {
+		next(error);
+	}
+};
+
+module.exports = {
+	login
+};
---
+++ controllers/user.js
@@ -0,0 +1,29 @@
+const User = require('../models/user.model');
+const { Exception } = require('../utils');
+const isEmail = require('validator/lib/isEmail');
+const { httpCodes } = require('../utils/constant');
+
+const registerUser = async (req, res, next) => {
+	try {
+		const { email, username, password } = req.body;
+		// check email valid
+		if (!isEmail(email)) throw new Exception('invalid email');
+		// check existed: username & email
+		const [ isExistedEmail, isExistedUsername ] = await Promise.all([
+			User.exists({ email }),
+			User.exists({ username })
+		]);
+		if (isExistedEmail) throw new Exception('email existed');
+		if (isExistedUsername) throw new Exception('username existed');
+		// create new user
+		const user = new User({ email, username, password });
+        await user.save();
+		return res.status(httpCodes.OK).send({message: 'register successful'});
+	} catch (error) {
+		next(error);
+	}
+};
+
+module.exports = {
+    registerUser
+}
