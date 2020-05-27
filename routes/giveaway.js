72c7b40b89e9aa4fb905787203b66026ac640f1d
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2020-05-27 22:57:15 +0700

    add giveaway route

---
+++ routes/giveaway.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const controller = require('../controllers/giveaway')
+const authMiddleware = require('../middlewares/auth')
+
+router.get('/', controller.getActiveGiveaways)
+router.get('/:id/registrations', controller.getRegistrationsOfGiveaway)
+router.post('/', authMiddleware.isAuthorized, controller.addGiveaway)
+router.patch('/:id', authMiddleware.isAuthorized, controller.updateWinnerInfo)
+router.patch('/:id/start', authMiddleware.isAuthorized, controller.startGiveaway)
+router.delete('/:id', authMiddleware.isAuthorized, controller.removeGiveaway)
+
+module.exports = router
